962d1fe0a425fd3d7edb61a077b5f0cbda0bede8
Brikarl <627639128@qq.com>
2019-10-18 00:14:43 +0800

    10.18challenge

---
+++ 06_Equal Sides Of An Array/Find_even_index.py
@@ -0,0 +1,44 @@
+'''
+You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+
+For example:
+
+Let's say you are given the array {1,2,3,4,3,2,1}:
+Your function will return the index 3, because at the 3rd position of the array, the sum of left side of the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
+
+Let's look at another one.
+You are given the array {1,100,50,-51,1,1}:
+Your function will return the index 1, because at the 1st position of the array, the sum of left side of the index ({1}) and the sum of the right side of the index ({50,-51,1,1}) both equal 1.
+
+Last one:
+You are given the array {20,10,-80,10,10,15,35}
+At index 0 the left side is {}
+The right side is {10,-80,10,10,15,35}
+They both are equal to 0 when added. (Empty arrays are equal to 0 in this problem)
+Index 0 is the place where the left side and right side are equal.
+'''
+
+
+# My solution
+def find_even_index(arr):
+    #your code here
+    l = len(arr)
+    for i in range(l+1):
+        a = 0 # 局部变量
+        b = 0
+        for a1 in range(0, i):
+            a += arr[a1]
+        for b1 in range(i+1, l):
+            b += arr[b1]
+        if a == b:
+            return i
+    return -1
+
+
+# Best solution
+def find_even_index(arr):
+    for i in range(len(arr)):
+        if sum(arr[:i]) == sum(arr[i+1:]):
+            return i
+    return -1
+    
---
+++ 08_Persistent Bugger/Persistence.py
@@ -0,0 +1,33 @@
+'''
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, 
+which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example:
+
+ persistence(39) => 3  # Because 3*9 = 27, 2*7 = 14, 1*4=4
+                       # and 4 has only one digit.
+
+ persistence(999) => 4 # Because 9*9*9 = 729, 7*2*9 = 126,
+                       # 1*2*6 = 12, and finally 1*2 = 2.
+
+ persistence(4) => 0   # Because 4 is already a one-digit number.
+'''
+
+
+# My solution
+def persistence(n):
+    # your codei
+    b = 0
+    while len(str(n)) > 1:
+        b += 1
+        a = 1
+        for i in str(n):
+            a *= int(i)
+        n = a
+    return b
+
+
+# Best solution
+def narcissistic(value):
+    return value == sum(int(x) ** len(str(value)) for x in str(value))
+    
