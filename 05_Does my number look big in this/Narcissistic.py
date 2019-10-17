ae68f14995158357a22d4e878b50ecded03e8790
Brikarl <627639128@qq.com>
2019-10-17 17:20:37 +0800

    Add files via upload

--- 05_Does my number look big in this/Narcissistic.py
+++ 05_Does my number look big in this/Narcissistic.py
@@ -1,3 +1,16 @@
+'''
+A Narcissistic Number is a number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+
+For example, take 153 (3 digits):
+
+    1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+and 1634 (4 digits):
+
+    1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634
+'''
+
+
+# My solution
 def narcissistic( value ):
     # Code away
     number_of_digits = len(str(value))
@@ -8,3 +21,9 @@ def narcissistic( value ):
         return True
     else:
         return False
+
+
+# Best solution
+def narcissistic(value):
+    return value == sum(int(x) ** len(str(value)) for x in str(value))
+    
